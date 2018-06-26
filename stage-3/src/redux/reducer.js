d45e409bbd29ddb0d1086b532767b4a2f3c76b47
truongtony9 <truong99t@gmail.com>
2018-06-26 16:09:56 -0500

    update

--- stage-3/src/redux/reducer.js
+++ stage-3/src/redux/reducer.js
@@ -31,8 +31,8 @@ export default function reducer(state = initialState, action) {
 
     case REMOVE_FROM_SHOPPING_CART:
       let newArray = state.shoppingCart.slice();
-      newArray.splice(action.index, 1);
-      return Object.assign({}, { shoppingCart: newArray });
+      newArray.splice(action.payload, 1);
+      return Object.assign({}, state, { shoppingCart: newArray });
 
     default:
       return state;
