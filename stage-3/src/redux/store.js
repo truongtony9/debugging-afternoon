1ff6ae901a03873bf95b355ab44c548c79577462
truongtony9 <truong99t@gmail.com>
2018-06-26 15:17:37 -0500

    update

--- stage-3/src/redux/store.js
+++ stage-3/src/redux/store.js
@@ -1,4 +1,5 @@
-import reducer from './reducer';
-import reduxPromiseMiddleware from 'redux-promise-middleware';
+import { createStore, applyMiddleware } from "redux";
+import reducer from "./reducer";
+import reduxPromiseMiddleware from "redux-promise-middleware";
 
 export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
